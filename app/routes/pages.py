a6bb06fa03f22abadd318e67e99d9552f3e4338f
Yasmine Kouoch <ysminekouch02@gmail.com>
2026-05-02 09:02:46 +0100

    Add presentation slide deck at /presentation route

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -3,8 +3,13 @@ import os
 
 pages = Blueprint("pages", __name__)
 
+static_dir = os.path.join(os.path.dirname(__file__), "../../static")
+
+@pages.route("/presentation")
+def presentation():
+    return send_from_directory(static_dir, "presentation.html")
+
 @pages.route("/")
 @pages.route("/<path:path>")
 def index(path=""):
-    static_dir = os.path.join(os.path.dirname(__file__), "../../static")
     return send_from_directory(static_dir, "index.html")
